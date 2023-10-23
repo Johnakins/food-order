2b830f303093cee998a8c57dd70117a394d4e310
Johnakins <johnolawole1@gmail.com>
2023-10-23 22:11:48 +0100

    Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,7 @@ const Footer = () => {
         <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 uppercase text-red-500 flex items-center justify-between">
             <Link href="/" className="font-bold text-xl">Johnakins</Link>
             <p>© ALL RIGHTS RESERVED.</p>
+            {/* Footer */}
         </div>
     );
 };
