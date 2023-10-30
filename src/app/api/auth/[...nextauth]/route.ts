9ceb0683400a39e0a517df1d18f53087c4bf1a1f
Johnakins <johnolawole1@gmail.com>
2023-10-30 10:05:52 +0100

    fetching orders based on user or admin

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,22 @@
 import { NextAuthOptions } from "next-auth";
-import { getServerSession } from 'next-auth/next'
+import { getServerSession, User } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import NextAuth from "next-auth/next";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prismadb from "@/lib/prismadb"
+
+declare module "next-auth" {
+    interface Session {
+        user: User & {
+            isAdmin: Boolean
+        }
+    }
+}
+declare module "next-auth/jwt" {
+    interface JWT {
+        isAdmin: Boolean
+    }
+}
 
 const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prismadb),
@@ -15,6 +28,27 @@ const authOptions: NextAuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
     ],
+    session: {
+        strategy: "jwt"
+    },
+    callbacks: {
+        async session({ token, session }) {
+            if (token) {
+                session.user.isAdmin = token.isAdmin
+            }
+            return session
+        },
+        async jwt({ token }) {
+            const userInDb = await prismadb.user.findUnique({
+                where: {
+                    email: token.email!
+                }
+            })
+            token.isAdmin = userInDb?.isAdmin!
+            return token
+        }
+
+    }
 }
 
 export const getAuthSession = () => getServerSession(authOptions)
